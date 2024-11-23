8eef5f2d0a234dc524bbee9f3a24abbcb3f11537
Vactor0911 <vactor0911@gmail.com>
2024-11-24 04:12:05 +0900

    index.ts 기본 틀 구성

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-console.log("Hello World");
-console.log("Ubuntu Server Testing");
-console.log("Test Message 1234");
+import express from "express";
+import cors from "cors";
+const PORT = 3000; // 서버가 실행될 포트 번호
+
+const app = express();
+app.use(cors());
+
+// 기본 라우트 설정
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// 서버 시작
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+});
