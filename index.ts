706430ae08c34a05d869f9278123e52769bbf97d
Vactor0911 <vactor0911@gmail.com>
2024-11-24 04:19:48 +0900

    index.ts 기본 틀 수정

--- index.ts
+++ index.ts
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
 });
 
 // 서버 시작
-app.listen(PORT, () => {
-  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
 });
