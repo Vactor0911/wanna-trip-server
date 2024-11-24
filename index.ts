b85985c2ad8aa25bcbbeedb3c8b919a5cdec360c
L-HITS <leechan753@gmail.com>
2024-11-24 14:08:45 +0900

    서버 시작 메세지에 접속 url 주소 추가

--- index.ts
+++ index.ts
@@ -7,10 +7,10 @@ app.use(cors());
 
 // 기본 라우트 설정
 app.get("/", (req, res) => {
-  res.send("Wanna Trip Web Server!@@@@");
+  res.send("Wanna Trip Web Server!");
 });
 
 // 서버 시작
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
+  console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.\n http://localhost:${PORT}/`);
 });
