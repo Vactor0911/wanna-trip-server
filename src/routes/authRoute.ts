40dd446b9446ef379be4ee5f1e7834c1ec39d9c2
Vactor0911 <vactor0911@gmail.com>
2025-03-27 10:42:52 +0900

    백엔드 소스코드 리팩토링

---
+++ src/routes/authRoute.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { googleLogin, kakaoLogin, login, logout, register } from "../controllers/authController";
+
+const authRoute = express.Router();
+
+authRoute.post("/login", login);
+authRoute.post("/login/kakao", kakaoLogin);
+authRoute.post("/login/google", googleLogin);
+authRoute.post("/register", register);
+authRoute.post("logout", logout);
+
+export default authRoute;
