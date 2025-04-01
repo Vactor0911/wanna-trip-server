e6636c83146c75983a2689d9ae19aa8ae24ac24c
L-HITS <leechan753@gmail.com>
2025-04-01 18:38:57 +0900

    비밀번호 재설정, 이메일 인증 코드 전송, 이메인 인증 코드 확인 API 추가

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { googleLogin, kakaoLogin, login, logout, register } from "../controllers/authController";
+import { googleLogin, kakaoLogin, login, logout, register, resetPassword, sendVerifyEmail, verifyEmailCode } from "../controllers/authController";
+import { csrfProtection } from "../utils";
 
 const authRoute = express.Router();
 
@@ -8,4 +9,12 @@ authRoute.post("/login/kakao", kakaoLogin);
 authRoute.post("/login/google", googleLogin);
 authRoute.post("/register", register);
 authRoute.post("/logout", logout);
+
+// 비밀번호 재설정 관련
+authRoute.patch("/resetPassword", csrfProtection, resetPassword);
+
+// 이메일 인증 관련
+authRoute.post("/sendVerifyEmail", sendVerifyEmail);
+authRoute.post("/verifyEmailCode", verifyEmailCode);
+
 export default authRoute;
