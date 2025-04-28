47af0ab846bd5c957b65f2dd6bedbf732c8231a1
L-HITS <leechan753@gmail.com>
2025-04-28 10:11:34 +0900

    라우터 수정

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import { googleLogin, kakaoLogin, login, logout, register, resetPassword, sendVerifyEmail, verifyEmailCode } from "../controllers/authController";
+import { googleLogin, kakaoLogin, login, logout, refreshToken, register, resetPassword, sendVerifyEmail, verifyEmailCode } from "../controllers/authController";
 import { csrfProtection } from "../utils";
 
 const authRoute = express.Router();
 
-authRoute.post("/login", login);
+authRoute.post("/login", csrfProtection, login); // 완
 authRoute.post("/login/kakao", kakaoLogin);
 authRoute.post("/login/google", googleLogin);
-authRoute.post("/register", register);
-authRoute.post("/logout", csrfProtection, logout);
+authRoute.post("/register", register);  // 완
+authRoute.post("/logout", csrfProtection, logout); // 일반 로그아웃까지는 끝
 
 // 비밀번호 재설정 관련
 authRoute.patch("/resetPassword", csrfProtection, resetPassword);
@@ -16,5 +16,8 @@ authRoute.patch("/resetPassword", csrfProtection, resetPassword);
 // 이메일 인증 관련
 authRoute.post("/sendVerifyEmail", sendVerifyEmail);
 authRoute.post("/verifyEmailCode", verifyEmailCode);
+
+// 리프레쉬 토큰
+authRoute.post("/token/refresh", csrfProtection, refreshToken); // 테스트 안해봄
 
 export default authRoute;
