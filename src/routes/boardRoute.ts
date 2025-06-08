88d69d7685c45d180d365ee2af009d76c961a47e
L-HITS <leechan753@gmail.com>
2025-06-08 18:36:15 +0900

    보드 라우터 수정

--- src/routes/boardRoute.ts
+++ src/routes/boardRoute.ts
@@ -3,10 +3,7 @@ import { authenticateToken } from "../middleware/authenticate";
 import { csrfProtection, limiter } from "../utils";
 import { 
   createBoard, 
-  updateBoard, 
   deleteBoard,
-  getBoardDetail,
-  reorderBoards,
   duplicateBoard,
   createBoardAfter
 } from "../controllers/boardController";
@@ -17,22 +14,13 @@ const boardRoute = express.Router();
 boardRoute.use(csrfProtection);
 
 // 새 보드 생성 (맨 뒤에 생성)
-boardRoute.post("/:templateId", limiter, authenticateToken, createBoard);
+boardRoute.post("/:templateUuid", limiter, authenticateToken, createBoard);
 
 // 특정 보드 뒤에 새 보드 생성
 boardRoute.post("/after/:boardId", limiter, authenticateToken, createBoardAfter);
 
-// 보드 정보 조회
-boardRoute.get("/:boardId", limiter, authenticateToken, getBoardDetail);
-
-// 보드 수정
-boardRoute.put("/:boardId", limiter, authenticateToken, updateBoard);
-
 // 보드 삭제
 boardRoute.delete("/:boardId", limiter, authenticateToken, deleteBoard);
-
-// 보드 순서 변경
-boardRoute.put("/reorder/:templateId", limiter, authenticateToken, reorderBoards);
 
 // 보드 복제 (특정 보드 바로 뒤에 복제)
 boardRoute.post("/duplicate/:boardId", limiter, authenticateToken, duplicateBoard);
