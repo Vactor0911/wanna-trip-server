e3d16c1bf56589df2b630dc73fef8d946f188f8d
L-HITS <leechan753@gmail.com>
2025-06-11 01:54:27 +0900

    미연동 API 주석 처리

--- src/routes/cardRoute.ts
+++ src/routes/cardRoute.ts
@@ -3,13 +3,7 @@ import { authenticateToken } from "../middleware/authenticate";
 import { csrfProtection, limiter } from "../utils";
 import {
   createCard,
-  getCard,
   updateCard,
-  deleteCard,
-  reorderCards,
-  duplicateCard,
-  moveCard,
-  searchCards
 } from "../controllers/cardController";
 
 const router = express.Router();
@@ -20,26 +14,26 @@ router.use(csrfProtection);
 // 새 카드 생성
 router.post("/:boardId", limiter, authenticateToken, createCard);
 
-// 카드 조회
-router.get("/:cardId", limiter, authenticateToken, getCard);
-
-// 카드 수정
+// 카드 수정 (텍스트 에디터 내용 업데이트 포함)
 router.put("/:cardId", limiter, authenticateToken, updateCard);
 
-// 카드 삭제
-router.delete("/:cardId", limiter, authenticateToken, deleteCard);
+// // 카드 조회
+// router.get("/:cardId", limiter, authenticateToken, getCard);
 
-// 카드 순서 변경
-router.put("/reorder/:boardId", limiter, authenticateToken, reorderCards);
+// // 카드 삭제
+// router.delete("/:cardId", limiter, authenticateToken, deleteCard);
 
-// 카드 복제
-router.post("/duplicate/:cardId", limiter, authenticateToken, duplicateCard);
+// // 카드 순서 변경
+// router.put("/reorder/:boardId", limiter, authenticateToken, reorderCards);
 
-// 카드 이동
-router.put("/move/:cardId", limiter, authenticateToken, moveCard);
+// // 카드 복제
+// router.post("/duplicate/:cardId", limiter, authenticateToken, duplicateCard);
 
-// 카드 검색
-router.get("/search/:templateId", limiter, authenticateToken, searchCards);
+// // 카드 이동
+// router.put("/move/:cardId", limiter, authenticateToken, moveCard);
+
+// // 카드 검색
+// router.get("/search/:templateId", limiter, authenticateToken, searchCards);
 
 
 export default router;
