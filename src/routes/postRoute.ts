eb23e9ce0a97353216dc6328c509700a57dbc65c
L-HITS <leechan753@gmail.com>
2025-06-21 18:41:41 +0900

    UUID 로 게시글 조회 API 구현

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { csrfProtection, limiter, refreshTokenLimiter } from "../utils";
 import { authenticateToken } from "../middleware/authenticate";
+import { getPostByUuid } from "../controllers/postController";
 
 const postRoute = express.Router();
 
-
+// UUID로 게시글 조회 (로그인 없이도 조회 가능)
+postRoute.get("/:postUuid", limiter, getPostByUuid);
 
 
 
