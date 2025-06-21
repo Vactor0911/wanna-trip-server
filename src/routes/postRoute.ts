ece7c65f98abe7e41a65d52cd5c6d41b58ecd27e
L-HITS <leechan753@gmail.com>
2025-06-21 20:25:23 +0900

    게시글 / 댓글 좋아요 토글 추가

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import { csrfProtection, limiter } from "../utils";
 import { authenticateToken } from "../middleware/authenticate";
-import { createComment, deleteComment, deletePost, getCommentsByPostUuid, getPostByUuid } from "../controllers/postController";
+import {
+  createComment,
+  deleteComment,
+  deletePost,
+  getCommentsByPostUuid,
+  getPostByUuid,
+  toggleLike,
+} from "../controllers/postController";
 
 const postRoute = express.Router();
 
@@ -9,16 +16,42 @@ const postRoute = express.Router();
 postRoute.get("/:postUuid", limiter, getPostByUuid);
 
 // 게시글 삭제
-postRoute.delete("/:postUuid", limiter, authenticateToken, csrfProtection, deletePost);
+postRoute.delete(
+  "/:postUuid",
+  limiter,
+  authenticateToken,
+  csrfProtection,
+  deletePost
+);
 
 // 게시글의 댓글 목록 조회 (로그인 없이도 조회 가능)
 postRoute.get("/comments/:postUuid", limiter, getCommentsByPostUuid);
 
 // 댓글 작성
-postRoute.post("/comments/:postUuid", limiter, authenticateToken, csrfProtection, createComment);
+postRoute.post(
+  "/comments/:postUuid",
+  limiter,
+  authenticateToken,
+  csrfProtection,
+  createComment
+);
 
 // 댓글 삭제 (본인 댓글 또는 게시글 작성자가 삭제 가능)
-postRoute.delete("/comments/:commentUuid", limiter, authenticateToken, csrfProtection, deleteComment);
+postRoute.delete(
+  "/comments/:commentUuid",
+  limiter,
+  authenticateToken,
+  csrfProtection,
+  deleteComment
+);
 
+// 게시글 / 댓글 좋아요 토글
+postRoute.post(
+  "/likes/:targetType/:targetUuid",
+  limiter,
+  authenticateToken,
+  csrfProtection,
+  toggleLike
+);
 
 export default postRoute;
