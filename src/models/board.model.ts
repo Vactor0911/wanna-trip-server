04a7c28e73b074c068a6ae974d680da549a615f1
Vactor0911 <vactor0911@gmail.com>
2025-11-03 22:49:44 +0900

    보드 생성 파라미터에 보드 uuid 추가

--- src/models/board.model.ts
+++ src/models/board.model.ts
@@ -2,6 +2,7 @@ import { Pool, PoolConnection } from "mariadb";
 import { dbPool } from "../config/db";
 
 type CreateBoardParams = {
+  board_uuid: string;
   templateId: string;
   dayNumber: number;
 };
@@ -68,11 +69,11 @@ class BoardModel {
     params: CreateBoardParams,
     connection: PoolConnection | Pool = dbPool
   ) {
-    const { templateId, dayNumber } = params;
+    const { board_uuid, templateId, dayNumber } = params;
 
     const result = await connection.execute(
-      `INSERT INTO board (template_id, day_number) VALUES (?, ?)`,
-      [templateId, dayNumber]
+      `INSERT INTO board (board_uuid, template_id, day_number) VALUES (?, ?, ?)`,
+      [board_uuid, templateId, dayNumber]
     );
     return result;
   }
